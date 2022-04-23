efb7b44eb54ed3f1db11c3d59496dcd2a462a0c4
CrazyMax <crazy-max@users.noreply.github.com>
2022-04-23 14:35:24 +0200

    chore: update dev dependencies and workflow
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- __tests__/dagger.test.ts
+++ __tests__/dagger.test.ts
@@ -1,22 +1,20 @@
-import fs = require('fs');
+import {describe, expect, it} from '@jest/globals';
+import * as fs from 'fs';
 import * as dagger from '../src/dagger';
 
 describe('install', () => {
   it('acquires latest version of Dagger', async () => {
     const daggerBin = await dagger.install('latest');
-    console.log(daggerBin);
     expect(fs.existsSync(daggerBin)).toBe(true);
   }, 100000);
 
   it('acquires latest 0.1 version', async () => {
     const daggerBin = await dagger.install('0.1');
-    console.log(daggerBin);
     expect(fs.existsSync(daggerBin)).toBe(true);
   }, 100000);
 
   it('acquires 0.1.0-alpha.9 version of Dagger', async () => {
     const daggerBin = await dagger.install('0.1.0-alpha.9');
-    console.log(daggerBin);
     expect(fs.existsSync(daggerBin)).toBe(true);
   }, 100000);
 });
--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,9 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
-  testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
-  verbose: false
-}
+  verbose: true
+};
