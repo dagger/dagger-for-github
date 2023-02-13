66757b4966f527fcabc428b208727bb92342a64b
Marcos Nils <1578458+marcosnils@users.noreply.github.com>
2023-02-13 17:08:12 -0300

    rename dagger to dagger-cue (#79)
    * rename dagger to dagger-cue
    fixes #78
    
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- __tests__/dagger.test.ts
+++ __tests__/dagger.test.ts
@@ -10,18 +10,13 @@ describe('build', () => {
 });
 
 describe('install', () => {
-  it('acquires latest version of Dagger', async () => {
+  it('acquires latest version of dagger-cue', async () => {
     const daggerBin = await dagger.install('latest');
     expect(fs.existsSync(daggerBin)).toBe(true);
   }, 100000);
 
-  it('acquires latest 0.1 version', async () => {
-    const daggerBin = await dagger.install('0.1');
-    expect(fs.existsSync(daggerBin)).toBe(true);
-  }, 100000);
-
-  it('acquires 0.1.0-alpha.9 version of Dagger', async () => {
-    const daggerBin = await dagger.install('0.1.0-alpha.9');
+  it('acquires latest 0.2.232 version', async () => {
+    const daggerBin = await dagger.install('0.2.232');
     expect(fs.existsSync(daggerBin)).toBe(true);
   }, 100000);
 });
