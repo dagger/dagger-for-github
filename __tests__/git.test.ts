82365fb655b27b5651b4b7b349962d97f32727e5
Jeremy Adams <jeremy@dagger.io>
2023-11-13 13:55:25 -0800

    chore: Set aside old content
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- __tests__/git.test.ts
+++
@@ -1,9 +0,0 @@
-import {describe, expect, it} from '@jest/globals';
-import * as git from '../src/git';
-
-describe('git', () => {
-  it('returns git remote ref', async () => {
-    const ref: string = await git.getRemoteSha('https://github.com/dagger/dagger.git', 'refs/pull/2161/head');
-    expect(ref).toEqual('aeb8ea3973a7815fff7cbd16f986811baa08ae2f');
-  });
-});
--- test/README.md
+++
@@ -1,8 +0,0 @@
-```console
-dagger init
-cp ~/.config/dagger/keys.txt .
-dagger new ci -p ./ci
-mkdir -p ./ci
-dagger query
-dagger up --no-cache --log-format plain
-```
